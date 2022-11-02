dcd0e732429ebbb88feaba5e4a6b571128873109
Jude Obinna <45057355+Obinnajude@users.noreply.github.com>
2022-11-02 13:23:34 -0500

    Update settings.py

--- authapp/settings.py
+++ authapp/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j_3^dnt=kvf-na58m=wwl14l)@@ye6k-^c)s3w*zuj*gvve168'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
